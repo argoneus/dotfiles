a767503f0cb3149ab4e610b468b41a8f3f7777bb
Nick Davis <nick@nickdavis.name>
2019-02-09 19:58:29 -0600

    Add z.lua installation

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@
 # 10-6-12 by Nick Davis
 
 scriptdir=$PWD
+codedir=~/code
+scriptsdir=~/scripts
 for cfg in `ls -d .??*` 
 do
 	echo "current file: $cfg"
@@ -28,3 +30,41 @@ do
 		echo "File ~/$cfg exists, so not symlinked"
 	fi
 done
+
+# install z.lua directory matching tool
+if [ ! -e "$codedir" ]; then
+	echo "Creating $codedir"
+	mkdir $codedir 
+fi
+if [ ! -e "$scriptsdir" ]; then
+	echo "Creating $scriptsdir"
+	mkdir $scriptsdir
+fi
+
+# clone github repo
+if [ ! -e "$codedir/z.lua" ]; then
+	echo "Cloning repo"
+	cd $codedir
+	git clone https://github.com/skywind3000/z.lua
+fi
+# create symbolic link to ~/scripts
+if [ ! -e "$scriptsdir/z.lua" ]; then
+	echo "Symlinking z.lua"
+	cd $scriptsdir
+	ln -s $codedir/z.lua/z.lua .
+fi
+
+# add to .bashrc
+if [ ! -e ~/.bashrc ]; then
+	echo "Creating .bashrc"
+	touch ~/.bashrc
+fi
+
+if ! grep -q "z.lua" ~/.bashrc; then
+	echo "Appending z.lua to ~/.bashrc"
+	str1='eval "$(lua '
+	str2=$scriptsdir/z.lua
+	str3=' --init bash enhance once)"'
+	echo "$str1$str2$str3" >> ~/.bashrc
+fi
+
